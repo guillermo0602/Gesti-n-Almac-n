cd251d656b7d112b524d9137eb6a94aa27b4fa43
guillermo0602 <gui_w_17@hotmail.com>
2025-09-17 19:51:21 +0200

    inserción de HashMap

--- src/Inventario.java
+++ src/Inventario.java
@@ -3,11 +3,17 @@ import java.util.HashMap;
 public class Inventario {
     private HashMap<String,Producto>inventario;
 
-    public Inventario() {
+    public Inventario(String nombre) {
         this.inventario=new HashMap<>();
     }
 
-    public void agregarProducto(Producto producto) {
-        inventario.put(producto.getCodigo(),producto);
+    public void agregarProducto(String Codigo,Producto producto) {
+        if(!inventario.containsKey(Codigo)) {
+            inventario.put(Codigo,producto);
+            System.out.println("El producto se ha agregado correctamente");
+        }else{
+            System.out.println("ERROR");
+        }
     }
+
 }
